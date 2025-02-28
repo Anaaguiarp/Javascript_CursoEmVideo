911091119f66d7322b2f8b2a9eb615b06af82285
Ana Carolina A Pereira <anacarolinaaguiarp@gmail.com>
2025-02-28 00:20:54 -0300

    Add files via upload

---
+++ Aula 11/ex006.js
@@ -0,0 +1,7 @@
+var vel = 10
+console.log(`A Velocidade do seu carro é ${vel}Km/h`)
+
+if(vel > 60){// condição simples
+    console.log(`Você ultrapassou a velocidade permitida, MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
